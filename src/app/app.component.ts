6e55e95400fc672899cf305615d92feee5de8b79
Anjo Tadena <anjo@coderepubliq.com>
2017-07-23 18:37:09 +0800

    Setting up local variable viewChild.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterContentChecked, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterContentChecked {
   title = 'app';
+  @ViewChild('boundParagraph') boundParagraph: HTMLElement;
+  ngOnInit() {
+  	// console.log(boundParagraph);
+  }
+
+  ngAfterContentChecked() {
+  	console.log(this.boundParagraph);
+  }
 }
