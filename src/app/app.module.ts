80a87f18983e90f0820cc4ed413611de8f614a4e
Anjo Tadena <anjo@coderepubliq.com>
2017-07-23 13:54:38 +0800

    Setup event binding.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventBindingComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/event-binding/event-binding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-binding',
+  templateUrl: './event-binding.component.html',
+  styleUrls: ['./event-binding.component.css']
+})
+export class EventBindingComponent {
+	onClick () {
+		alert('Event handling worked!');
+	}
+}
