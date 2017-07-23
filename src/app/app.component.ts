e130f265ced09b51bca4446232ce61686de3fb0d
Anjo Tadena <anjo@coderepubliq.com>
2017-07-23 18:05:15 +0800

    Setting up angular lifecycle hook.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  delete = false;
+  boundableValue = 1000;
 }
--- src/app/lifecycle/lifecycle.component.ts
+++ src/app/lifecycle/lifecycle.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+	Component, 
+	OnInit, 
+	OnChanges, 
+	DoCheck, 
+	AfterContentInit, 
+	AfterContentChecked, 
+	AfterViewInit, 
+	AfterViewChecked, 
+	OnDestroy,
+	Input
+} from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
   styleUrls: ['./lifecycle.component.css']
 })
-export class LifecycleComponent implements OnInit {
-
+export class LifecycleComponent implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+  @Input() bindable = 1000;
   constructor() { }
 
-  ngOnInit() {
+  ngOnChanges() {
+  	this.log('ngOnChanges');
   }
 
+  ngOnInit() {
+  	this.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+  	this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+  	this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+  	this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+  	this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+  	this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+  	this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+  	console.log(hook);
+  }
 }
